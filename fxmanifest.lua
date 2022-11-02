2995e3ea61e94ef5bfe3a255352b3e948d2a5943
MOWGLI777 <98538633+MOWGLI777@users.noreply.github.com>
2022-11-02 21:13:46 +0530

    added 2 guns
    added browning and dp9

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+---------------------------------------------------------------------------
+-- INCLUDED FILES
+---------------------------------------------------------------------------
+files {
+	'weaponcomponents.meta',
+	'pedpersonality.meta',
+	'weaponanimations.meta',
+	'weaponarchetypes.meta',
+	'weapon_dp9.meta',
+	'weapon_browning.meta',
+	'weapon_staff.meta',
+	'weapon_ltl.meta'
+	}
+
+---------------------------------------------------------------------------
+-- DATA FILES
+---------------------------------------------------------------------------
+	data_file 'WEAPONCOMPONENTSINFO_FILE' 'weaponcomponents.meta'
+	data_file 'PED_PERSONALITY_FILE' 'pedpersonality.meta'
+	data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+	data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+	data_file 'WEAPONINFO_FILE' 'weapon_dp9.meta'
+	data_file 'WEAPONINFO_FILE' 'weapon_browning.meta'
+	data_file 'WEAPONINFO_FILE' 'weapon_ltl.meta'
+	data_file 'WEAPONINFO_FILE' 'weapon_staff.meta'
